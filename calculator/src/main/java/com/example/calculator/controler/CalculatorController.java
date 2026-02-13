2ab05c2d816d63240c47f8f848609bf4cf5ea4f3
Andriy <andriybondarenko1999@gmail.com>
2026-02-13 11:59:29 +0200

    Add calculator REST API

---
+++ calculator/src/main/java/com/example/calculator/controler/CalculatorController.java
@@ -0,0 +1,38 @@
+package com.example.calculator.controler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    @GetMapping("/add")
+    public int add(@RequestParam int a, @RequestParam int b){
+
+        return a + b;
+    }
+
+    @GetMapping("/subtract")
+    public int subtract(@RequestParam int a, @RequestParam int b){
+
+        return a - b;
+    }
+
+    @GetMapping("/multiply")
+    public int multiply(@RequestParam int a, @RequestParam int b){
+
+        return a * b;
+    }
+
+    @GetMapping("/divide")
+    public String divide(@RequestParam int a, @RequestParam int b){
+        if (b == 0){
+            return "На нуль ділити не можна";
+        }
+
+        int result = a / b;
+        return " "  + result;
+    }
+
+}
